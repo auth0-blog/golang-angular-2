e93e858e0eb1a9058f532c45861951bd6c996a79
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-14 16:35:07 -0300

    ### Creating the To-Do Component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule

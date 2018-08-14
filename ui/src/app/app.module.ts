7e1e4e1a4d6139d7ebd1a526068634c6f9c016f7
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-14 17:25:24 -0300

    ### Setting up Routing and Securing Angular with Auth0

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,24 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuardService } from './auth-guard.service';
+import { AuthService } from './auth.service';
+import { CallbackComponent } from 'src/app/callback/callback.component';
 import { TodoComponent } from './todo/todo.component';
-import { CallbackComponent } from './callback/callback.component';
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
-import {TokenInterceptor} from "./token.interceptor";
+import { TodoService } from './todo.service';
+import { FormsModule } from '@angular/forms';
+import { TokenInterceptor } from './token.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TodoComponent,
-    CallbackComponent
+    CallbackComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [{
+  providers: [AuthGuardService, AuthService, TodoService, {
     provide: HTTP_INTERCEPTORS,
     useClass: TokenInterceptor,
     multi: true

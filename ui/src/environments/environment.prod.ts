2bccb0e28e17381ca9dd4ce31ec9a108828d846f
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-14 10:49:42 -0300

    ### Initializing the Angular Project

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  gateway: '',
+  callback: 'http://localhost:4200/callback',
+  domain: '<YOUR_AUTH0_TENANT>.auth0.com',
+  clientId: '<YOUR_AUTH0_APPLICATION_CLIENT_ID>'
 };
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,15 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  gateway: 'http://localhost:3000',
+  callback: 'http://localhost:4200/callback',
+  domain: '<YOUR_AUTH0_TENANT>.auth0.com',
+  clientId: '<YOUR_AUTH0_APPLICATION_CLIENT_ID>'
 };
-
-/*
- * In development mode, for easier debugging, you can ignore zone related error
- * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
- * because it will have a performance impact when errors are thrown
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.

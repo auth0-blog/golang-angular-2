0a067b002cce6e3214a8b30b75d78989b239e1c6
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-14 17:23:16 -0300

    before updating appmodule

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TodoComponent } from './todo/todo.component';
+import { CallbackComponent } from './callback/callback.component';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {TokenInterceptor} from "./token.interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TodoComponent
+    TodoComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   gateway: '',
   callback: 'http://localhost:4200/callback',
   domain: 'bk-tmp.auth0.com',
-  clientId: 'z4Z09quobJQ7ngXTaUDkCzzQ9vjZ6eEX'
+  clientId: 'z4Z09quobJQ7ngXTaUDkCzzQ9vjZ6eEX',
+  audience: 'https://my-golang-api'
 };
